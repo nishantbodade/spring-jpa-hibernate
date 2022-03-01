b23ff271eeda5dcef703871f573de12b64626c9f
Nishant Bodade <nishantbodade@gmail.com>
2022-03-01 10:52:33 +0530

    service tear is created for user Autowire the userservice in userController and pass the User refactore the interface from userserviceimpl

---
+++ service/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.pluralsight.conference.service;
+
+import com.pluralsight.conference.model.User;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User save(User user){
+        return null;
+    }
+}
